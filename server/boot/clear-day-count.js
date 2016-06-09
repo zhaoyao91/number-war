541d8c0bce830359cd95e5d79bf31db2b89d654e
zhaoyao91 <zhaoyao91@163.com>
2016-06-09 23:11:11 +0800

    use cron

--- server/boot/clear-day-count.js
+++ server/boot/clear-day-count.js
@@ -1,12 +1,12 @@
-import schedule from 'node-schedule';
 import Users from '../../common/collections/users';
 import {Meteor} from 'meteor/meteor';
+import {CronJob} from 'cron';
 
 export default function () {
     if (process.env.CLEAR_DAY_COUNTS) {
-        schedule.scheduleJob('0 0 0 * * *', Meteor.bindEnvironment(()=> {
+        new CronJob('0 0 0 * * *', Meteor.bindEnvironment(()=>{
             console.log('clear day counts of users at ' + new Date);
             Users._collection.update({}, {$set: {dayWinCount: 0, dayLoseCount: 0, dayScore: 0}}, {multi: true});
-        }))
+        }), null, true)
     }
 }
