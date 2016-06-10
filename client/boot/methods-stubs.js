a5cd158e6f3bd0239a9c2753ea04449231df1349
zhaoyao91 <zhaoyao91@163.com>
2016-06-10 19:13:27 +0800

    add delay compensation

---
+++ client/boot/methods-stubs.js
@@ -0,0 +1,98 @@
+import Users from '../../common/collections/users';
+import {Meteor} from 'meteor/meteor';
+import gameUtils from '../../common/utils/game';
+import {check} from 'meteor/check';
+import {DDPRateLimiter} from 'meteor/ddp-rate-limiter';
+import _ from 'lodash';
+
+export default function () {
+    Meteor.methods({
+        'Game.fight'(targetUserId) {
+            check(targetUserId, String);
+
+            // check login
+            const user = Meteor.user();
+            if (!user) {
+                throw new Meteor.Error('not-login', 'user is not logged-in');
+            }
+
+            // find target user
+            const targetUser = Users.findOne({_id: targetUserId});
+            if (!targetUser) {
+                throw new Meteor.Error('no-target-user', 'cannot find target user');
+            }
+
+            if (user._id === targetUser._id) {
+                throw new Meteor.Error('same-user', 'user cannot fight with itself');
+            }
+
+            // fight the game
+            const win = gameUtils.fight(user.fightMode, user.number, targetUser.number);
+            let winner, loser;
+            if (win) {
+                winner = user;
+                loser = targetUser;
+            }
+            else {
+                winner = targetUser;
+                loser = user;
+            }
+
+            const now = new Date;
+
+            // update winner
+            const winnerSet = {
+                number: gameUtils.updateWinNumber(winner.number, loser.number),
+                fightMode: gameUtils.getRandomFightMode(),
+                fightOrder: now.getTime() - _.result(winner, 'lastLoseAt.getTime', 0),
+                lastWinAt: now,
+                lastFight: {
+                    result: 'win',
+                    nickname: loser.nickname,
+                    fightAt: now
+                },
+                score: winner.winCount + 1 - winner.loseCount,
+                dayScore: winner.dayWinCount + 1 - winner.dayLoseCount
+            };
+            const winnerInc = {
+                winCount: 1,
+                dayWinCount: 1
+            };
+            Users.update({_id: winner._id}, {
+                $set: winnerSet,
+                $inc: winnerInc
+            }, err=>err && console.error(err));
+
+            // update loser
+            const loserSet = {
+                number: gameUtils.updateLoseNumber(loser.number, winner.number),
+                fightMode: gameUtils.getRandomFightMode(),
+                fightOrder: _.result(loser, 'lastWinAt.getTime', 0) - now.getTime(),
+                lastLoseAt: now,
+                lastFight: {
+                    result: 'lose',
+                    nickname: winner.nickname,
+                    fightAt: now
+                },
+                score: loser.winCount - (loser.loseCount + 1),
+                dayScore: loser.dayWinCount - (loser.dayLoseCount + 1)
+            };
+            const loserInc = {
+                loseCount: 1,
+                dayLoseCount: 1
+            };
+            Users.update({_id: loser._id}, {
+                $set: loserSet,
+                $inc: loserInc
+            }, err=>err && console.error(err));
+
+            return win;
+        }
+    });
+
+    DDPRateLimiter.addRule({
+        userId: userId=>true,
+        type: 'method',
+        name: 'Game.fight'
+    }, 1, 1000);
+}
